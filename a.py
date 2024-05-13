940e0f88f5a046af9e3752c345201601334ca031
bkaspb <bkaspb@bkaspbs-MacBook-Pro.local>
2024-05-13 14:23:43 +0200

    first commit

---
+++ a.py
@@ -0,0 +1,15 @@
+#connect to ftp
+import ftplib
+# import config config.cfg and include
+# ftp_host, ftp_user, ftp_password
+import configparser
+config = configparser.ConfigParser()
+config.read('config.cfg')
+ftp_host = config['ftp']['ftp_host']
+ftp_user = config['ftp']['ftp_user']
+ftp_password = config['ftp']['ftp_password']
+
+ftp = ftplib.FTP(ftp_host)
+ftp.login(ftp_user,ftp_password)
+ftp.retrlines('LIST')
+ftp.quit()
